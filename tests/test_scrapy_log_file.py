4e65a37a2be07614c049e650cd2bbcad60d52ce5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-27 16:59:29 -0400

    chore(style): Use set for set inclusion condition

--- tests/test_scrapy_log_file.py
+++ tests/test_scrapy_log_file.py
@@ -46,8 +46,8 @@ def test_find(files, expected, tmpdir):
         file.write(content)
 
     if expected:
-        assert ScrapyLogFile.find(tmpdir, "source_id", data_version).name in (
-            directory.join(name) for name in expected
+        assert any(
+            ScrapyLogFile.find(tmpdir, "source_id", data_version).name == directory.join(name) for name in expected
         )
     else:
         assert ScrapyLogFile.find(tmpdir, "source_id", data_version) is None
